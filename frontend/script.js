3152930d3e027ea9670ff85165cb7b4e7fdf9510
MythicBeastHaven <160296361+MythicBeastHaven@users.noreply.github.com>
2024-02-18 12:45:28 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const twitchBtn = document.querySelector('.twitch-btn');
-    twitchBtn.addEventListener('click', function(event) {
-        event.preventDefault();
-        const twitchAuthUrl = 'https://id.twitch.tv/oauth2/authorize?client_id=9mv1pwbxjq33qyftx2xlsu821bwmnd&redirect_uri=https://sites.google.com/view/mbh-prueba&response_type=code&force_verify=false&scope=openid%20user:read:email%20chat:read%20moderator:read:chatters&state=sample_string&claims=%7B%22userinfo%22:%7B%22picture%22:null,%20%22email%22:null,%20%22name%22:null,%20%22user%22:%20null,%20%22preferred_username%22:%20null%7D%7D';
-        window.location.href = twitchAuthUrl;
+$(document).ready(function() {
+    Twitch.init({clientId: '9mv1pwbxjq33qyftx2xlsu821bwmnd'}, function(error, status) {
+        if (error) {
+            console.log(error);
+        }
+        if (status.authenticated) {
+            $('.twitch-connect').hide();
+        }
+    });
+
+    $('.twitch-connect').click(function() {
+        Twitch.login({
+            scope: ['user_read', 'channel_read']
+        });
     });
 });
